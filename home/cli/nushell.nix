002c43a5b2cc8878a890f59fecd52594ec05aa79
VictorBuch <victorbuch@protonmail.com>
2025-11-27 14:51:31 +0100

    feat: add nushell aliases

--- home/cli/nushell.nix
+++ home/cli/nushell.nix
@@ -15,6 +15,11 @@ let
     n = "nvim ~/serenityOs/ ";
     y = "yazi";
     s = "sesh connect (sesh list --icons | fzf --ansi)";
+    nr = "npm run ";
+    ni = "npm install";
+    dcu = "docker compose up -d";
+    dcd = "docker compose down";
+    nrc = "npm run prettier; npm run lint; npm run ts-check";
   };
 in
 

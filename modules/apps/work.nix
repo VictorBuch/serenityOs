7ecc274e96a7b7dee2463beb4594ef4e822911ad
VictorBuch <victorbuch@protonmail.com>
2026-01-07 12:34:15 +0100

    update nixvim for flutter emulators

--- modules/apps/work.nix
+++ modules/apps/work.nix
@@ -18,8 +18,17 @@ mkApp {
     pkgs.prisma
     pkgs.openssl
   ];
-  extraConfig = {
+  linuxExtraConfig = {
     environment.sessionVariables = {
+      PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+      PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+      PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
+      PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+      PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
+    };
+  };
+  darwinExtraConfig = {
+    launchd.user.envVariables = {
       PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
       PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
       PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
--- modules/homelab/services/gitea.nix
+++ modules/homelab/services/gitea.nix
@@ -129,7 +129,7 @@ in
           tokenFile = config.sops.templates."gitea-runner-env".path;
           labels = [
             "node:docker://node:20-bookworm"
-	    "ubuntu-latest:docker://ghcr.io/catthehacker/ubuntu:full-latest"
+            "ubuntu-latest:docker://ghcr.io/catthehacker/ubuntu:full-latest"
           ];
           settings = {
             container = {
@@ -153,12 +153,12 @@ in
             curl
             git
             nix
-	    go
-	    openssh
-	    gzip
-	    gnutar
-	    nodejs
-	    sudo
+            go
+            openssh
+            gzip
+            gnutar
+            nodejs
+            sudo
           ];
         };
       };
@@ -204,7 +204,7 @@ in
       DynamicUser = lib.mkForce false;
       User = lib.mkForce "wanna-share-releaser";
       Group = lib.mkForce "wanna-share-releaser";
-      NoNewPrivileges = lib.mkForce false;  # Required for sudo
+      NoNewPrivileges = lib.mkForce false; # Required for sudo
     };
 
   };

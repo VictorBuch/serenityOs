5e76208df79099b1be472d825399860438734db0
VictorBuch <victorbuch@protonmail.com>
2026-05-27 14:44:00 +0200

    feat: android stuido

--- modules/apps/development/editors/android-studio.nix
+++ modules/apps/development/editors/android-studio.nix
@@ -3,6 +3,7 @@ args@{ config, pkgs, lib, mkModule, ... }:
 mkModule {
   name = "android-studio";
   category = "development";
-  linuxPackages = {pkgs, ...}: [pkgs.android-studio];
   description = "android studio";
+  linuxPackages = { pkgs, ... }: [ pkgs.androidStudioPackages.canary ];
+  casks = [ "android-studio" ];
 } args
